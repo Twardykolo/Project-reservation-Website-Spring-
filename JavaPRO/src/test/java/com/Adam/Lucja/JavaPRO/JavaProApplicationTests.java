a1890ddbea2ffbb5555e22147888c257fb4387d7
Twardykolo <adamus8895@gmail.com>
2022-01-17 21:44:15 +0100

    Start

---
+++ JavaPRO/src/test/java/com/Adam/Lucja/JavaPRO/JavaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Adam.Lucja.JavaPRO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
