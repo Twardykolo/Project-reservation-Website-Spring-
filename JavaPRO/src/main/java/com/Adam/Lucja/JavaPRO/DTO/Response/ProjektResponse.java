d6aec72ff6e424ef191c0d8b63b0804d5d830d02
Twardykolo <adamus8895@gmail.com>
2022-01-20 10:05:37 +0100

    update(ProjektResponse)

--- JavaPRO/src/main/java/com/Adam/Lucja/JavaPRO/DTO/Response/ProjektResponse.java
+++ JavaPRO/src/main/java/com/Adam/Lucja/JavaPRO/DTO/Response/ProjektResponse.java
@@ -1,6 +1,7 @@
 package com.Adam.Lucja.JavaPRO.DTO.Response;
 
 import com.Adam.Lucja.JavaPRO.Entity.File;
+import com.Adam.Lucja.JavaPRO.Entity.Projekt;
 import com.Adam.Lucja.JavaPRO.Entity.Temat;
 import lombok.*;
 
@@ -18,4 +19,11 @@ public class ProjektResponse {
     private Double mark;
     private Temat temat;
 
+    public ProjektResponse(Projekt projekt){
+        this.id= projekt.getId();
+        this.submissionDate=projekt.getSubmissionDate();
+        this.deadline=projekt.getDeadline();
+        this.mark= projekt.getMark();
+        this.temat=projekt.getTemat();
+    }
 }
