c962136ff91507343507e6f616d43ac5cd0613d4
Twardykolo <adamus8895@gmail.com>
2022-01-22 10:24:10 +0100

    Możliwość sortowania tematów

--- JavaPRO/src/main/java/com/Adam/Lucja/JavaPRO/DTO/Response/TematResponse.java
+++ JavaPRO/src/main/java/com/Adam/Lucja/JavaPRO/DTO/Response/TematResponse.java
@@ -3,12 +3,14 @@ package com.Adam.Lucja.JavaPRO.DTO.Response;
 import com.Adam.Lucja.JavaPRO.Entity.Temat;
 import lombok.*;
 
+import java.util.Comparator;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class TematResponse {
+public class TematResponse implements Comparator<TematResponse> {
     private Long id;
     private String name;
     private String description;
@@ -19,5 +21,8 @@ public class TematResponse {
         this.description= temat.getDescription();
         this.isReserved=temat.getIsReserved();
         this.name= temat.getName();
+    }
+    public int compare(TematResponse temat1, TematResponse temat2){
+        return temat1.getId().compareTo(temat2.getId());
     }
 }
