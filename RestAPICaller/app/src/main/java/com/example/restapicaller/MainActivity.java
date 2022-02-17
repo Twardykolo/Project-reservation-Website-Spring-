50b5d5706c6b979a4e9b5f2aefe20c2431747440
Twardykolo <adamus8895@gmail.com>
2022-02-17 20:21:17 +0100

    Apka mobilna do pobierania listy tematów

---
+++ RestAPICaller/app/src/main/java/com/example/restapicaller/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.restapicaller;
+
+import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.restapicaller.databinding.ActivityMainBinding;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_first);
+        text = (TextView) findViewById(R.id.textview_first);
+        text.setMovementMethod(new ScrollingMovementMethod());
+//        sendRequest();
+    }
+
+    public void sendRequest(View view){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.10.5:8080/tematy/";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                            Type type = new TypeToken<List<Temat>>() {}.getType();
+                            List<Temat> lista = new Gson().fromJson(response,type);
+                            String wynik = "";
+                            for(Temat temat: lista){
+                                wynik=wynik+temat.toString();
+                            }
+                            text.setText(wynik);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                text.setText(error.toString());
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+}
---
+++ RestAPICaller/app/src/main/java/com/example/restapicaller/Temat.java
@@ -0,0 +1,89 @@
+package com.example.restapicaller;
+
+public class Temat {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean isReserved;
+    private Integer liczbaOsob;
+    private String deadline;
+
+    @Override
+    public String toString() {
+        return "Nazwa tematu: "+name+'\n'+
+                "Opis tematu: "+description+'\n'+
+                "Czy został zarezerwowany: "+isReserved+'\n'+
+                "Termin ostateczny: "+deadline+'\n'+
+                "\n";
+
+
+    }
+//    @Override
+//    public String toString() {
+//        return "Temat{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", description='" + description + '\'' +
+//                ", isReserved=" + isReserved +
+//                ", liczbaOsob=" + liczbaOsob +
+//                ", deadline='" + deadline + '\'' +
+//                '}'+'\n';
+//    }
+
+    public Temat(Long id, String name, String description, Boolean isReserved, Integer liczbaOsob, String deadline) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isReserved = isReserved;
+        this.liczbaOsob = liczbaOsob;
+        this.deadline = deadline;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getReserved() {
+        return isReserved;
+    }
+
+    public void setReserved(Boolean reserved) {
+        isReserved = reserved;
+    }
+
+    public Integer getLiczbaOsob() {
+        return liczbaOsob;
+    }
+
+    public void setLiczbaOsob(Integer liczbaOsob) {
+        this.liczbaOsob = liczbaOsob;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+}
